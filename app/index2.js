4ea2218a76b2968a9a733b2c07c77c94b40b58a9
Dmitry <zadima@gmail.com>
2019-09-26 19:20:09 +0300

    check global object

--- app/index2.js
+++
@@ -1,6 +0,0 @@
-const collection = ['some', 'awesome', 'collection']
-collection = ['some', 'overridden', 'collection']
-
-_.map(collection, () => { });
-
-['i`m', 'native', 'map'].map(word => word)
